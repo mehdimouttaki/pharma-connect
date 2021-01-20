a17bd2eef7e261b83b55990c832f72b5e5ac83a5
mehdimouttaki <mehdimouttaki99@gmail.com>
2021-01-20 17:40:45 +0100

    init project

---
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/PharmaconnectApplication.java
@@ -0,0 +1,13 @@
+package ma.pharmaconnect.app.pharmaconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaconnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/pharmaconnect/app/pharmaconnect/PharmaconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.pharmaconnect.app.pharmaconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
