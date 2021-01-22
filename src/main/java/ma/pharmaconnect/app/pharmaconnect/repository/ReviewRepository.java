59be5b4e5d54d79331928a76e9a8517bc12c6fdd
Youssef <youssef.najeh@smals.be>
2021-01-22 20:52:10 +0100

    add review repository

--- src/main/java/ma/pharmaconnect/app/pharmaconnect/repository/ReviewRepository.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/repository/ReviewRepository.java
@@ -1,10 +1,10 @@
 package ma.pharmaconnect.app.pharmaconnect.repository;
 
-import ma.pharmaconnect.app.pharmaconnect.model.Admin;
+import ma.pharmaconnect.app.pharmaconnect.model.Review;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminRepository extends CrudRepository<Admin, Integer> {
+public interface ReviewRepository extends CrudRepository<Review, Integer> {
 
 }
