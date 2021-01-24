24f52e5fbab56f67bc18760f3184a3b9af44c02f
Youssef <youssef.najeh@smals.be>
2021-01-24 13:49:42 +0100

    add client save and delete endpoint

--- src/main/java/ma/pharmaconnect/app/pharmaconnect/controller/api/ClientRestController.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/controller/api/ClientRestController.java
@@ -1,7 +1,23 @@
 package ma.pharmaconnect.app.pharmaconnect.controller.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import ma.pharmaconnect.app.pharmaconnect.model.Client;
+import ma.pharmaconnect.app.pharmaconnect.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ClientRestController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping("/api/clients")
+    public Client createClient(@RequestBody Client client){
+        return clientService.save(client);
+    }
+
+    @DeleteMapping("/api/clients/{id}")
+    public void deleteClient(@PathVariable Integer id){
+        clientService.delete(id);
+    }
 }
--- src/main/java/ma/pharmaconnect/app/pharmaconnect/service/ClientService.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/service/ClientService.java
@@ -1,7 +1,21 @@
 package ma.pharmaconnect.app.pharmaconnect.service;
 
+import ma.pharmaconnect.app.pharmaconnect.model.Client;
+import ma.pharmaconnect.app.pharmaconnect.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public Client save(Client client) {
+        return clientRepository.save(client);
+    }
+
+    public void delete(Integer id) {
+        clientRepository.deleteById(id);
+    }
 }
