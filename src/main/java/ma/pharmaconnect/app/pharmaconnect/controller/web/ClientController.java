9f2fd2fb8c991473f8b23a8447507b77764a5bc5
Youssef <youssef.najeh@smals.be>
2021-01-28 19:57:24 +0100

    add delivery and client endpoint controller web

--- src/main/java/ma/pharmaconnect/app/pharmaconnect/controller/web/ClientController.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/controller/web/ClientController.java
@@ -1,7 +1,13 @@
 package ma.pharmaconnect.app.pharmaconnect.controller.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientController {
+
+    @GetMapping("/clients")
+    public String allClients() {
+        return "all_clients";
+    }
 }
--- src/main/java/ma/pharmaconnect/app/pharmaconnect/controller/web/DeliveryManController.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/controller/web/DeliveryManController.java
@@ -1,7 +1,23 @@
 package ma.pharmaconnect.app.pharmaconnect.controller.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DeliveryManController {
+
+    @GetMapping("/delivery-man/add")
+    public String addDeliveryMan() {
+        return "add_delivery_men";
+    }
+
+    @GetMapping("/delivery-men")
+    public String allDeliveryMan() {
+        return "all_delivery_men";
+    }
+
+    @GetMapping("/delivery-men/edit")
+    public String editDeliveryMan() {
+        return "edit_delivery_men";
+    }
 }
