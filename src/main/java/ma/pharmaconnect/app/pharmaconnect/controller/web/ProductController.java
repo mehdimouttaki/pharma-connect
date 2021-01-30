c6765127df404b82242e94c3b6d5ffa9ac68ea15
mehdimouttaki <mehdimouttaki99@gmail.com>
2021-01-30 22:12:29 +0100

    Add Product In Menu and add list Product

--- src/main/java/ma/pharmaconnect/app/pharmaconnect/controller/web/ProductController.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/controller/web/ProductController.java
@@ -1,13 +1,38 @@
 package ma.pharmaconnect.app.pharmaconnect.controller.web;
 
 
+import ma.pharmaconnect.app.pharmaconnect.model.DeliveryMan;
+import ma.pharmaconnect.app.pharmaconnect.model.Product;
+import ma.pharmaconnect.app.pharmaconnect.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
+    @Autowired
+    ProductService productService;
     @GetMapping("/products")
-    public String allProducts() {
+    public String allProduct(Model model) {
+        List<Product> list = productService.getAll();
+        model.addAttribute("products", list);
         return "/products/all_products";
+    }
+    @PostMapping("/products/add")
+    public String saveProduct(Product product) {
+        productService.save(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/delete/{id}")
+    public String deleteProduct(@PathVariable Integer id) {
+        productService.delete(id);
+        return "redirect:/products";
     }
 }
--- src/main/java/ma/pharmaconnect/app/pharmaconnect/service/ProductService.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/service/ProductService.java
@@ -1,9 +1,12 @@
 package ma.pharmaconnect.app.pharmaconnect.service;
 
+import ma.pharmaconnect.app.pharmaconnect.model.DeliveryMan;
 import ma.pharmaconnect.app.pharmaconnect.model.Product;
 import ma.pharmaconnect.app.pharmaconnect.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -19,5 +22,10 @@ public class ProductService {
     }
     public Product update(Product product){
         return productRepository.save(product);
+    }
+
+    public List<Product> getAll() {
+        return (List<Product>) productRepository.findAll();
+
     }
 }
