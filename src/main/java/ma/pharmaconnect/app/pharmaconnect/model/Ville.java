4a7eb164b05698c806aae687d4d58bba85ff3e4c
mehdimouttaki <mehdimouttaki99@gmail.com>
2021-02-04 19:17:41 +0100

    Update  Script V8 in  table ville(region) And Update Class ville

--- src/main/java/ma/pharmaconnect/app/pharmaconnect/model/Ville.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/model/Ville.java
@@ -4,12 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 @Setter
 @Getter
 @ToString
 public class Ville {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String ville;
-    private Integer region;
+
 }
