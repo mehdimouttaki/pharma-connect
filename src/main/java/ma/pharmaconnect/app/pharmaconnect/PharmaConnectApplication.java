57cc6ed302fa98cdff7cc8997da05ce897f1f5b5
Youssef <youssef.najeh@smals.be>
2021-02-08 19:27:08 +0100

    prepare to deploy

--- src/main/java/ma/pharmaconnect/app/pharmaconnect/PharmaConnectApplication.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/PharmaConnectApplication.java
@@ -2,12 +2,19 @@ package ma.pharmaconnect.app.pharmaconnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PharmaConnectApplication {
+public class PharmaConnectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PharmaConnectApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PharmaConnectApplication.class);
     }
 
 }
