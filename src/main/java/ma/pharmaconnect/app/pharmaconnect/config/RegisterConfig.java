39f3750d8204e81d7452ede6bcdbbae63acdf725
Youssef <youssef.najeh@smals.be>
2021-02-21 15:13:43 +0100

    remove last @Autowired

--- src/main/java/ma/pharmaconnect/app/pharmaconnect/config/RegisterConfig.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/config/RegisterConfig.java
@@ -1,18 +1,18 @@
 package ma.pharmaconnect.app.pharmaconnect.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class RegisterConfig implements WebMvcConfigurer {
 
-	@Autowired
-	private RestSecurityHandlerInterceptor restSecurityHandlerInterceptor;
+    private final RestSecurityHandlerInterceptor restSecurityHandlerInterceptor;
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(restSecurityHandlerInterceptor).addPathPatterns("/api/**");
-	}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(restSecurityHandlerInterceptor).addPathPatterns("/api/**");
+    }
 }
