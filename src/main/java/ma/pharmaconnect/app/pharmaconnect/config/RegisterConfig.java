4b978b43dce9565ce9bee6ab6e1e45d15e64c8ce
Youssef <youssef.najeh@smals.be>
2021-02-21 21:54:58 +0100

    integration of flyway

--- src/main/java/ma/pharmaconnect/app/pharmaconnect/config/RegisterConfig.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/config/RegisterConfig.java
@@ -1,15 +1,14 @@
 package ma.pharmaconnect.app.pharmaconnect.config;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class RegisterConfig implements WebMvcConfigurer {
-
-    private final RestSecurityHandlerInterceptor restSecurityHandlerInterceptor;
+    @Autowired
+    private RestSecurityHandlerInterceptor restSecurityHandlerInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
