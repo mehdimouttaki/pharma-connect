159d5f4006c0dd98d1ecfaa3310d133500eb4021
mehdimouttaki <mehdimouttaki99@gmail.com>
2021-02-22 22:16:04 +0100

    edit product

--- src/main/java/ma/pharmaconnect/app/pharmaconnect/controller/web/ProductController.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/controller/web/ProductController.java
@@ -4,6 +4,7 @@ package ma.pharmaconnect.app.pharmaconnect.controller.web;
 
 import lombok.RequiredArgsConstructor;
 import ma.pharmaconnect.app.pharmaconnect.dto.product.ProductCreationDTO;
+import ma.pharmaconnect.app.pharmaconnect.dto.product.ProductUpdateDTO;
 import ma.pharmaconnect.app.pharmaconnect.model.Product;
 import ma.pharmaconnect.app.pharmaconnect.service.ProductService;
 import org.modelmapper.ModelMapper;
@@ -25,22 +26,36 @@ public class ProductController {
     public String addProduct() {
         return "/products/add_products";
     }
+    @PostMapping("/products/add")
+    public String saveProduct(ProductCreationDTO productDTO) {
+        Product product=new ModelMapper().map(productDTO,Product.class);
+        productService.save(product);
+        return "redirect:/products";
+    }
     @GetMapping("/products")
     public String allProduct(Model model) {
         List<Product> list = productService.getAll();
         model.addAttribute("products", list);
         return "/products/all_products";
     }
-    @PostMapping("/products/add")
-    public String saveProduct(ProductCreationDTO productDTO) {
-        Product product=new ModelMapper().map(productDTO,Product.class);
-        productService.save(product);
-        return "redirect:/products/all_products";
-    }
+
 
     @GetMapping("/products/delete/{id}")
     public String deleteProduct(@PathVariable Integer id) {
         productService.delete(id);
+        return "redirect:/products";
+    }
+    @GetMapping("/products/edit/{id}")
+    public String editProduct(Model model, @PathVariable Integer id) {
+        Product product = productService.getOne(id);
+        model.addAttribute("product", product);
+        return "/products/edit_products";
+    }
+
+    @PostMapping("/product/edit")
+    public String updateProduct(ProductUpdateDTO productDTO) {
+        Product product = new ModelMapper().map(productDTO, Product.class);
+        productService.save(product);
         return "redirect:/products";
     }
 }
