54d3ee778636fd9d1434cc87933f3d15b004134a
Youssef N <youssef.najeh@gmail.com>
2021-03-03 17:39:30 +0100

    only admin can access web app

--- src/main/java/ma/pharmaconnect/app/pharmaconnect/config/MyUserDetailsService.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/config/MyUserDetailsService.java
@@ -1,7 +1,10 @@
 package ma.pharmaconnect.app.pharmaconnect.config;
 
 import lombok.RequiredArgsConstructor;
+import ma.pharmaconnect.app.pharmaconnect.model.Admin;
 import ma.pharmaconnect.app.pharmaconnect.model.User;
+import ma.pharmaconnect.app.pharmaconnect.model.UserRole;
+import ma.pharmaconnect.app.pharmaconnect.repository.AdminRepository;
 import ma.pharmaconnect.app.pharmaconnect.repository.UserRepository;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,12 +18,12 @@ import java.util.Collections;
 @Component
 @RequiredArgsConstructor
 public class MyUserDetailsService  implements UserDetailsService {
-   private final UserRepository userRepository;
+   private final AdminRepository adminRepository;
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(userName)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format("user not exist %s ", userName)));
+        Admin user = adminRepository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("admin not exist %s ", userName)));
         Collection<SimpleGrantedAuthority> roles = Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name()));;
         return new   org.springframework.security.core.userdetails.User(user.getUsername(),  user.getPassword(), roles);
     }
--- src/main/java/ma/pharmaconnect/app/pharmaconnect/repository/AdminRepository.java
+++ src/main/java/ma/pharmaconnect/app/pharmaconnect/repository/AdminRepository.java
@@ -4,8 +4,12 @@ import ma.pharmaconnect.app.pharmaconnect.model.Admin;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends CrudRepository<Admin, Integer> {
 
     boolean existsByUsername(String username);
+
+    Optional<Admin> findByUsername(String username);
 }
